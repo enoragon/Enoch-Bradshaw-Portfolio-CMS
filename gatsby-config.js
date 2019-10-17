926287a00c81e3b834bd45a83c13494ff00629a5
Nucky <enoragon@gmail.com>
2019-10-17 12:43:00 -0500

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,13 @@ module.exports = {
         ],
       },
     },
-
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-150288055-1",
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
